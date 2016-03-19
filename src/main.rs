d5cd0a2b5600d75f128450295a6eab7632a970df
yodalee <lc85301@gmail.com>
2016-03-19 23:12:26 +0800

    add time measurement code using crate Time

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+extern crate time;
+
+use time::precise_time_ns;
+
 fn main() {
     println!("rendering ...");
+    let start_time = precise_time_ns();
+    let end_time = precise_time_ns();
     println!("Done");
+
+    println!("Running time: {} seconds", (end_time - start_time) as f64 / 10e9);
 }
