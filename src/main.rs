8298dc0bfe5e129e84f1ae1e2d3e34f78303031a
yodalee <lc85301@gmail.com>
2016-03-19 23:28:54 +0800

    save image with crate bmp

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
 extern crate time;
+extern crate bmp;
 
 use time::precise_time_ns;
+use bmp::{Image, Pixel};
+
+const WIDTH: u32 = 640;
+const HEIGHT: u32 = 480;
 
 fn main() {
     println!("rendering ...");
     let start_time = precise_time_ns();
+
+    let mut img = Image::new(WIDTH, HEIGHT);
+
+    for (x, y) in img.coordinates() {
+        img.set_pixel(x, y, Pixel{r: 127, g: 127, b: 127});
+    }
+
+    let _ = img.save("scene.bmp");
+
     let end_time = precise_time_ns();
     println!("Done");
 
