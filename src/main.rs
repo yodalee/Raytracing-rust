1ea4475d76803008332adca165d7fd3145f6bb8f
yodalee <lc85301@gmail.com>
2016-03-20 09:18:57 +0800

    add xamnt, yamnt calculation

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,9 @@ use light::{Light};
 
 const WIDTH: u32 = 640;
 const HEIGHT: u32 = 480;
+const FWIDTH: f64 = WIDTH as f64;
+const FHEIGHT: f64 = HEIGHT as f64;
+const ASPECT: f64 = FWIDTH / FHEIGHT;
 
 fn main() {
     println!("rendering ...");
@@ -26,9 +29,10 @@ fn main() {
     let mut img = Image::new(WIDTH, HEIGHT);
 
     // declare X, Y, Z unit vector
-    let X = Vec3::new(1.0, 0.0, 0.0);
-    let Y = Vec3::new(0.0, 1.0, 0.0);
-    let Y = Vec3::new(0.0, 0.0, 1.0);
+    let O = Vec3::new(0.0, 0.0, 0.0);
+    let X: Vec3<f64> = Vec3::x();
+    let Y: Vec3<f64> = Vec3::y();
+    let Y: Vec3<f64> = Vec3::z();
 
     // declare camera
     let campos = Vec3::new(2.0, 1.5, -4.0);
@@ -43,14 +47,30 @@ fn main() {
     // declare light
     let white_light = Rgba::new(1.0, 1.0, 1.0, 0.0);
     let pretty_green = Rgba::new(0.5, 1.0, 0.5, 0.3);
+    let maroon = Rgba::new(0.5, 0.25, 0.25, 0.0);
     let gray = Rgba::new(0.5, 0.5, 0.5, 0.0);
     let black = Rgba::new(0.0, 0.0, 0.0, 0.0);
 
     let light_position = Vec3::new(-7.0, 10.0, -10.0);
     let scene_light = Light::new(light_position, white_light);
 
+    let mut xamnt: f64 = 0.0;
+    let mut yamnt: f64 = 0.0;
+
     for (x, y) in img.coordinates() {
-        img.set_pixel(x, y, Pixel{r: 127, g: 127, b: 127});
+        // start with no-AA
+        let fx = x as f64;
+        let fy = y as f64;
+        if ASPECT > 1.0 {
+            xamnt = ((fx+0.5)/FWIDTH)*ASPECT - ((FWIDTH-FHEIGHT) / FHEIGHT / 2f64);
+            yamnt = ((FHEIGHT - fy) + 0.5)/FHEIGHT;
+        } else if ASPECT < 1.0 {
+            xamnt = (fx+0.5)/FWIDTH;
+            yamnt = (((FHEIGHT - fy)+0.5)/ FHEIGHT)/ASPECT - ((FHEIGHT-FWIDTH)/ FWIDTH/2.0);
+        } else {
+            xamnt = (fx+0.5)/FWIDTH;
+            yamnt = ((FHEIGHT - fy) + 0.5)/FHEIGHT;
+        }
     }
 
     let _ = img.save("scene.bmp");
